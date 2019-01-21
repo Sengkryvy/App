005b78a63f6709343969b4f48ac6c90e206cd66e
SengkryVY <sengkryvy@gmail.com>
2019-01-21 19:03:08 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,17 +2,42 @@ $(document).ready(function () {
 
     $("#play").click(function () {
         if ($(this).attr("src").toString().indexOf('source/img/icons8-play-30.png') != -1) {
-
-            function pause();
+    
             this.src = this.src.replace("source/img/icons8-play-30.png", "source/img/icons8-pause-30.png");
+            // function pause();
 
         } else {
 
-            function play();
             this.src = this.src.replace("source/img/icons8-pause-30.png", "source/img/icons8-play-30.png");
+            // function play();
 
         }
+
 
     });
 
 });
+
+function play() {
+
+}
+
+function pause() {
+
+}
+
+function next() {
+
+}
+
+function prev() {
+
+}
+
+function shuffle() {
+
+}
+
+function repeat() {
+
+}
