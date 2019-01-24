3389e5f0d69ce556afb369720fcf178fbc773e6e
SengkryVY <sengkryvy@gmail.com>
2019-01-24 18:17:51 +0700

    adding to explore page

--- js/main.js
+++ js/main.js
@@ -1,32 +1,103 @@
+
+  
+  
+  // USE LIKE:
+  
+
+
 $(document).ready(function () {
+    jQuery.fn.clickToggle = function(a,b) {
+        var ab = [b,a];
+        return this.on("click", function(){ ab[this._tog^=1].call(this); });
+      };
 
-    $("#play").click(function () {
-        if ($(this).attr("src").toString().indexOf('source/img/icons8-play-30.png') != -1) {
+    $(".track-card").clickToggle(function playSound() {   
+        // alert("AAA");
+        b.play();
+   }, function() {
+        // alert("BBB");
+        b.pause();
+   });
 
-            this.src = this.src.replace("source/img/icons8-play-30.png", "source/img/icons8-pause-30.png");
-            // function pause();
+    // $("#card, .track-card").clicl( function () {
+    //     alert("First handler for .toggle() called.");
+    // }, function () {
+    //     alert("Second handler for .toggle() called.");
+    // });
 
-        } else {
+    // $('#card').click(function () {
+    //         alert("FIRST handler for .toggle() called.");
+    //     },
+    //     function () {
+    //         alert("Second handler for .toggle() called.");
+    //     });
 
-            this.src = this.src.replace("source/img/icons8-pause-30.png", "source/img/icons8-play-30.png");
-            // function play();
-
-        }
-
+    $("#toggle").click(function () {
+        // function playSound();
+        $("aside").toggleClass("aside");
+        $("#content").toggleClass("ml-0 col-md-12 col-xl-12 col-lg-12");
+        // $("#content").toggleClass("offset-0, offset-md-0, offset-lg-0, offset-xl-0, offset-sm-0, col-sm-12 col-12 col-md-6 col-lg-6 col-xl-12");
+        $("#search").toggleClass("ml-md-4")
+        // $("#search").toggleClass("ml-50")
 
     });
 
+    // $("#play").click(function () {
+    //     if ($(this).attr("src").toString().indexOf('source/img/icons8-play-30.png') != -1) {
+
+    //         this.src = this.src.replace("source/img/icons8-play-30.png", "source/img/icons8-pause-30.png");
+    //         // function pause();
+
+    //     } else {
+
+    //         this.src = this.src.replace("source/img/icons8-pause-30.png", "source/img/icons8-play-30.png");
+    //         // function play();
+
+    //     }
+
+
 });
 
-$("#toggle").click(function () {
-    $("aside").toggleClass("aside");
-    $("#content").toggleClass("ml-0 col-md-12 col-xl-12 col-lg-12");
+// $('#audio')[0].play();
 
-    // $("#content").toggleClass("offset-0, offset-md-0, offset-lg-0, offset-xl-0, offset-sm-0, col-sm-12 col-12 col-md-6 col-lg-6 col-xl-12");
-    $("#search").toggleClass("ml-md-4")
-    // $("#search").toggleClass("ml-50")
+// $('#audio').on("play", function (me) {
+//     $('audio').each(function (i, e) {
+//         if (e !== me.currentTarget) {
+//             this.pause();
+//         }
+//     });
+// });
 
-});
+
+
+
+
+
+
+
+
+var a = new Audio('source/music/In The End - Linkin Park.mp3');
+var b = new Audio('https://firebasestorage.googleapis.com/v0/b/musicapp-4582e.appspot.com/o/In%20The%20End%20-%20Linkin%20Park.mp3?alt=media&token=8d74a9c1-77a5-4d38-baf4-d8bb4113f34f');
+function playSound() {
+    alert("hello")
+
+    a.play();
+}
+
+function pauseSound() {
+    // alert("hello")
+    a.pause();
+}
+
+
+
+// $("#card").click(
+//     function playSound () {
+//         // alert("hello")
+//         a.play();
+//         // $(this).css({"width": "100px"})
+//     }
+// );
 
 // $('#toggle').toggle(function () {
 //     $("#search").css({paddin: "0px"});
