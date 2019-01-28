34243d65d329ca0fd7ee7729338c5ff9a54f428e
SengkryVY <sengkryvy@gmail.com>
2019-01-28 13:01:32 +0700

    update aside bar

--- js/main.js
+++ js/main.js
@@ -1,40 +1,36 @@
-
 $(document).ready(function () {
-    jQuery.fn.clickToggle = function(a,b) {
-        var ab = [b,a];
-        return this.on("click", function(){ ab[this._tog^=1].call(this); });
-      };
+    jQuery.fn.clickToggle = function (a, b) {
+        var ab = [b, a];
+        return this.on("click", function () {
+            ab[this._tog ^= 1].call(this);
+        });
+    };
 
-    $(".track-card").clickToggle(function playSound() {   
-        // alert("AAA");
-        b.play();
-   }, function() {
-        // alert("BBB");
-        b.pause();
-   });
-
+    $(".track-card").clickToggle(
+        function () {
+            b.play();
+        },
+        function () {
+            b.pause();
+    });
 
     $("#toggle").click(function () {
-        // function playSound();
         $("aside").toggleClass("aside");
         $("#content").toggleClass("ml-0 col-md-12 col-xl-12 col-lg-12");
         $("#search").toggleClass("ml-md-4")
 
     });
 
+    $("aside ul li a").click(function() {
+        $("aside ul li").removeClass("active-menu");
+        $(this).parent().addClass("active-menu");
+        $(this).addClass("active-menu");
+        $("#weekly-chart").addClass("hide");
+    })
+
 
 });
 
-
-
-
-
-
-
-
-
-
-var a = new Audio('source/music/In The End - Linkin Park.mp3');
 var b = new Audio('https://firebasestorage.googleapis.com/v0/b/musicapp-4582e.appspot.com/o/In%20The%20End%20-%20Linkin%20Park.mp3?alt=media&token=8d74a9c1-77a5-4d38-baf4-d8bb4113f34f');
 
 function play() {
