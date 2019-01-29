e267931c1461355c2182aefe016d1cf17b43262a
SengkryVY <sengkryvy@gmail.com>
2019-01-29 12:06:36 +0700

    Add more reaction with bottom bar

--- js/main.js
+++ js/main.js
@@ -12,20 +12,48 @@ $(document).ready(function () {
         },
         function () {
             b.pause();
-    });
+        });
 
     $("#toggle").click(function () {
         $("aside").toggleClass("aside");
         $("#content").toggleClass("ml-0 col-md-12 col-xl-12 col-lg-12");
         $("#search").toggleClass("ml-md-4")
-
     });
 
-    $("aside ul li a").click(function() {
-        $("aside ul li").removeClass("active-menu");
+    //menu selection
+    $("aside ul li a").click(function () {
+        $("aside ul li, .active-menu").removeClass("active-menu");
         $(this).parent().addClass("active-menu");
         $(this).addClass("active-menu");
-        $("#weekly-chart").addClass("hide");
+    })
+
+    $("#menu-explore").click(function () {
+        $("#songs").addClass("hide");
+        $("#explore").removeClass("hide");
+    })
+
+    //display page respond to selected menu
+    $("#menu-songs").click(function () {
+        $("#songs").removeClass("hide");
+        $("#explore").addClass("hide");
+    })
+
+    //song selection
+    $(".list-songs table tbody tr").click(function () {
+
+        $(".list-songs table tbody tr").removeClass("current-song");
+        $(this).addClass("current-song");
+
+        var song = {
+            title:$(this.children[1]).text(),
+            artist:$(this.children[2]).text(),
+            album:$(this.children[3]).text(),
+            src:$(this.children[0].children[0].children[0]).css("background-image"),
+        }
+        console.log(song.title);
+        $(".play-artwork span").css({"background-image":song.src});
+        document.firstElementBu
+
     })
 
 
