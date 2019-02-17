fd895caab3ddcd05a7e497f0e58bd0e4f159b01d
SengkryVY <sengkryvy@gmail.com>
2019-02-17 13:24:01 +0700

    Create playAndControl.js

---
+++ js/playAndControl.js
@@ -0,0 +1,241 @@
+var prevSong = new Audio;
+var playingSong = null;
+var song;
+var add = 0;
+var current;
+
+var i = 0;
+var startTime = 0,
+    startMinutes = 0,
+    startSeconds = 0;
+var isPaused = false;
+
+
+$(document).ready(function () {
+    jQuery.fn.clickToggle = function (a, b) {
+        var ab = [b, a];
+        return this.on("click", function () {
+            ab[this._tog ^= 1].call(this);
+        });
+    };
+
+    //song selection in card
+    $(".track-card").click(function () {
+        current = $(this);
+
+        //set info to object song
+        setSongInCard();
+
+        playSong();
+
+        setPlayingInfo();
+    })
+
+    //song selection in list
+    $(".list-songs table tbody tr").click(function () {
+
+        // get and play song
+        // add += 1;
+        current = $(this)
+
+        //set info to object song
+        setSongInList(current);
+
+        playSong();
+
+        //set playing info in playing section
+        $(".list-songs table tbody tr").removeClass("current-song");
+        $(this).addClass("current-song");
+
+        setPlayingInfo();
+    })
+
+    // pause and play song
+    $("#play, #pause").click(function (e) {
+        var playingStatus = $(".play-progress .control #play i");
+        if (playingStatus.text() == "play_arrow") {
+            playingStatus.text("pause");
+            playingSong.play();
+
+            e.preventDefault();
+            isPaused = false;
+        } else {
+            playingStatus.text("play_arrow");
+            playingSong.pause();
+
+            e.preventDefault();
+            isPaused = true;
+        }
+    })
+
+    //go to next song
+    $("#next").click(function () {
+        var section = current.parent().parent().parent();
+        var temp;
+        //control for weekly chart list
+        if (section.parent().is('#weekly-chart')) {
+            if (current.is(":last-child")) {
+                current.removeClass("current-song");
+                current = current.parent().children(':first-child');
+            } else {
+                current = current.next();
+                $(".current-song").removeClass("current-song").next().addClass("current-song");
+            }
+        } else { //control for song list
+            if (current.is(":last-child")) {
+                current.removeClass("current-song");
+                current = current.parent().next().next().children(':first-child');
+                current.addClass("current-song");
+            } else {
+                current.removeClass("current-song");
+                current = current.next();
+                current.addClass("current-song");
+            }
+        }
+
+        setSongInList(current);
+        playSong();
+        setPlayingInfo();
+    })
+
+    //go to prev song
+    $("#prev").click(function () {
+        var section = current.parent().parent().parent();
+        var temp;
+        //control for weekly chart list
+        if (section.parent().is('#weekly-chart')) {
+            if (current.is(":first-child")) {
+                current.removeClass("current-song");
+                current = current.parent().children(':last-child');
+                current.addClass("current-song");
+            } else {
+                current = current.prev();
+                $(".current-song").removeClass("current-song").prev().addClass("current-song");
+            }
+        } else { //control for song list
+            if (current.is(":first-child")) {
+                current.removeClass("current-song");
+                current = current.parent().prev().prev().children(':last-child');
+                current.addClass("current-song");
+            } else {
+                current.removeClass("current-song");
+                current = current.prev();
+                current.addClass("current-song");
+            }
+        }
+
+        setSongInList(current);
+        playSong();
+        setPlayingInfo();
+    })
+
+    // track progress bar
+    var onePercentDuration = (song.duration / 100);
+    var endTime = song.duration;
+    var trackProgressBar = setInterval(function () {
+        if (!isPaused) {
+            $("#song-progress").css("width", i + "%");
+            i++; // incrementation
+            startTime = startTime + onePercentDuration;
+            // reset progress bar
+            if (i == 101) {
+                clearInterval(trackProgressBar);
+                i = 0;
+                $("#song-progress").css("width", i + "%");
+            }
+        }
+        // console.log(i);
+    }, onePercentDuration * 1000)
+
+    // track progress timer
+    var trackProgressTimer = setInterval(function () {
+        var endMinutes = Math.floor(song.duration / 60);
+        var endSeconds = Math.ceil(song.duration - minutes * 60);
+        // startMinutes = startSeconds = 0
+        if (!isPaused) {
+            startSeconds = startSeconds + 1; // incrementation
+            if (startSeconds < 10) {
+                $('#startDuration').text(startMinutes + ":0" + startSeconds);
+            } else {
+                if (startSeconds == 60) {
+                    startMinutes = startMinutes + 1;
+                    startSeconds = 0;
+                    $('#startDuration').text(startMinutes + ":" + startSeconds);
+                } else {
+                    $('#startDuration').text(startMinutes + ":" + startSeconds);
+                }
+            }
+            // console.log(startMinutes, startSeconds);
+            if (startMinutes == endMinutes && startSeconds == endSeconds) {
+                clearInterval(trackProgressTimer);
+                startMinutes = 0, startSeconds = 0;
+                $('#startDuration').text(startMinutes + ":" + startSeconds);
+            }
+        }
+    }, 1000)
+
+
+    // seeking (click on progress bar to go to a position)
+    var isSeeking = false;
+    var progressPosition = $("#song-progress").css("width");
+    // console.log(progressPosition);
+    $("#song-progress").click(function (e) {
+        var position = e.pageX - this.offsetLeft;
+        console.log(position);
+    });
+
+
+
+})
+
+function setSongInCard() {
+    song = {
+        title: $(current.children[1].children[0].children[0]).text(),
+        artist: $(current.children[1].children[1].children[0]).text(),
+        album: null,
+        duration: 0,
+        src: $(current).attr("src"),
+        cover: $(current.children[0].children[0]).css("background-image"),
+    };
+}
+
+function setSongInList(current) {
+    song = {
+        title: current.children(":nth-child(2)").text(),
+        artist: current.children(":nth-child(3)").text(),
+        album: current.children(":nth-child(4)").text(),
+        src: current.attr("src"),
+        duration: 0,
+        cover: current.children(":nth-child(1)").children(":nth-child(1)").children(":nth-child(1)").css("background-image"),
+    };
+}
+
+function setPlayingInfo() {
+
+    //set duration
+    $(playingSong).on("canplay", function () {
+        song = {
+            duration: this.duration,
+        };
+        var minutes = Math.floor(song.duration / 60);
+        var seconds = Math.ceil(song.duration - minutes * 60);
+        $('#duration').text(minutes + ":" + seconds);
+    });
+
+    //set playing info
+    $(".play-artwork span").css({
+        "background-image": song.cover
+    });
+    $(".play-info .info .title p").text(song.title);
+    $(".play-info .info .artist p").text(song.artist);
+    $(".play-progress .control #play i").text("pause");
+}
+
+function playSong() {
+    playingSong = new Audio(song.src);
+    prevSong.pause();
+    playingSong.play();
+    prevSong = playingSong;
+}
+
+function nextSong() {}
