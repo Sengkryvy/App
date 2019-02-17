1a1bcdcdfcadc1e6fafcdc48260ac640ef3309ec
KET Van Yarath <kvanyarath@gmail.com>
2019-02-17 12:46:00 +0700

    Track Song Progression

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+var prevSong = new Audio;
+var playingSong = null;
+var song;
+var i=0;
+var startTime = 0 ,startMinutes = 0, startSeconds = 0;
+var isPaused = false; 
+
+
 $(document).ready(function () {
     jQuery.fn.clickToggle = function (a, b) {
         var ab = [b, a];
@@ -16,7 +24,7 @@ $(document).ready(function () {
     //     console.log(datasnapshot.val());
     //     console.log($(list1[0].children[0].children[0].children[0]).css("background-image" , datasnapshot.val()));
     // })
-
+    
 
     $("#toggle").click(function () {
         $("aside").toggleClass("aside");
@@ -53,8 +61,7 @@ $(document).ready(function () {
     })
 
     //declara var for song selection
-    var prevSong = new Audio;
-    var playingSong = null;
+
 
     //select song in card
     $(".track-card").clickToggle(
@@ -69,7 +76,7 @@ $(document).ready(function () {
 
     //song selection in card
     $(".track-card").click(function () {
-        var song = {
+        song = {
             title: $(this.children[1].children[0].children[0]).text(),
             artist: $(this.children[1].children[1].children[0]).text(),
             album: null,
@@ -94,7 +101,7 @@ $(document).ready(function () {
     //song selection in list
     $(".list-songs table tbody tr").click(function () {
 
-        var song = {
+        song = {
             title: $(this.children[1]).text(),
             artist: $(this.children[2]).text(),
             album: $(this.children[3]).text(),
@@ -108,14 +115,62 @@ $(document).ready(function () {
         playingSong.play();
         prevSong = playingSong;
         var songDuration;
+        
         $(playingSong).on("canplay", function () {
             song = {duration: this.duration}
             var minutes = Math.floor(song.duration / 60);
             var seconds = Math.ceil(song.duration - minutes * 60);
             // console.log(minutes, seconds);
             $('#duration').text(minutes+":"+seconds);
-        });
+            
 
+            // track progress bar
+            var onePercentDuration=(song.duration/100);
+            var endTime = song.duration;
+            var trackProgressBar = setInterval(function () {
+                if (!isPaused){
+                    $("#song-progress").css("width",i+"%");
+                    i++; // incrementation
+                    startTime = startTime + onePercentDuration;
+                    // reset progress bar
+                    if (i == 101){
+                        clearInterval(trackProgressBar);
+                        i=0;
+                        $("#song-progress").css("width",i+"%");
+                    }
+                }
+                // console.log(i);
+            }, onePercentDuration*1000)
+            
+            // track progress timer
+            var trackProgressTimer = setInterval(function (){
+            var endMinutes = Math.floor(song.duration / 60);
+            var endSeconds = Math.ceil(song.duration - minutes * 60);
+            // startMinutes = startSeconds = 0
+                if (!isPaused){
+                    startSeconds = startSeconds + 1; // incrementation
+                    if (startSeconds < 10){
+                        $('#startDuration').text(startMinutes+":0"+startSeconds);
+                    }
+                    else{
+                        if (startSeconds == 60){
+                            startMinutes = startMinutes + 1;
+                            startSeconds = 0;
+                            $('#startDuration').text(startMinutes+":"+startSeconds);
+                        }
+                        else{
+                            $('#startDuration').text(startMinutes+":"+startSeconds);
+                        }
+                    }
+                    // console.log(startMinutes, startSeconds);
+                    if (startMinutes == endMinutes && startSeconds == endSeconds){
+                        clearInterval(trackProgressTimer);
+                        startMinutes = 0, startSeconds = 0;
+                        $('#startDuration').text(startMinutes+":"+startSeconds);
+                    }
+                }
+            }, 1000)
+        });
         
         $(".list-songs table tbody tr").removeClass("current-song");
         $(this).addClass("current-song");
@@ -130,18 +185,29 @@ $(document).ready(function () {
     })
 
     // pause song
-    $("#play, #pause").click(function () {
-
+    $("#play, #pause").click(function (e) {
         var playingStatus = $(".play-progress .control #play i");
         if (playingStatus.text() == "play_arrow") {
             playingStatus.text("pause");
             playingSong.play();
+            e.preventDefault();
+            isPaused = false;
         } else {
             playingStatus.text("play_arrow");
             playingSong.pause();
+            e.preventDefault();
+            isPaused = true;
         }
     })
 
+    // seeking (click on progress bar to go to a position)
+    var isSeeking = false;
+    var progressPosition = $("#song-progress").css("width");
+    // console.log(progressPosition);
+    $("#song-progress").click(function(e){
+        var position = e.pageX - this.offsetLeft;
+        console.log(position);
+    });
 
 });
 
